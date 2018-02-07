b07c6a363ade3e12468763426b4c2291f49882d9
bobby-pkt <bobby.puckett@pcc.edu>
2018-02-06 16:46:27 -0800

    Added hello message and current time

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hi. Walter!");
+        System.out.println("The current date is: ");
+        System.out.println(new Date());
+    }
 }
