56e4f579cd9b87c62fa8693b467489ddab855189
bobby-pkt <bobby.puckett@pcc.edu>
2018-02-06 18:12:12 -0800

    Removed extraneous message

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -10,7 +10,6 @@ public class HelloWorld {
         System.out.println(new Date());
 
         System.out.println(addNumbers(3, 4));
-        System.out.println("Extraneous line");
     }
 
     public static String addNumbers(int numA, int numB) {
